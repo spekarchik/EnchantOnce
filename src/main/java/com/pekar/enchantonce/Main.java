880608e9b02f0f3b4b81e13eaab00ee7c2738679
PEKAR\Sergey <sergey.pekarchik@gmail.com>
2024-06-23 02:25:08 +0200

    minor updates

--- src/main/java/com/pekar/enchantonce/Main.java
+++ src/main/java/com/pekar/enchantonce/Main.java
@@ -1,13 +1,17 @@
 package com.pekar.enchantonce;
 
+import com.mojang.logging.LogUtils;
 import com.pekar.enchantonce.events.EventRegistry;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.server.ServerStartingEvent;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
 import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
 
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(Main.MODID)
@@ -15,6 +19,7 @@ public class Main
 {
     // Directly reference a log4j logger.
     public static final String MODID = "enchantonce";
+    private static final Logger LOGGER = LogUtils.getLogger();
 
     //public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, MODID);
     //public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, MODID);
@@ -22,20 +27,24 @@ public class Main
 
     public Main()
     {
+        var bus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        // Register the setup method for modloading
+        //bus.addListener(this::commonSetup);
+        //bus.addListener(this::clientSetup);
+
         initializeRegistry();
 
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
         EventRegistry.registerEvents();
 
-        var bus = FMLJavaModLoadingContext.get().getModEventBus();
 //        BLOCKS.register(bus);
 //        ITEMS.register(bus);
 //        BLOCK_ENTITIES.register(bus);
 
-        // Register the setup method for modloading
-        //bus.addListener(this::commonSetup);
-        //bus.addListener(this::clientSetup);
+        // Register our mod's ForgeConfigSpec so that Forge can create and load the config file for us
+        //ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, Config.SPEC);
     }
 
     private void initializeRegistry()
