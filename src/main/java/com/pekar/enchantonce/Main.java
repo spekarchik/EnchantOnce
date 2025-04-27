3af0380d6d6387a3539d4c2bb3593a2735b9abce
PEKAR\Sergey <sergey.pekarchik@gmail.com>
2025-04-27 02:13:56 +0200

    cleanup

--- src/main/java/com/pekar/enchantonce/Main.java
+++ src/main/java/com/pekar/enchantonce/Main.java
@@ -18,45 +18,21 @@ import org.slf4j.Logger;
 @Mod(Main.MODID)
 public class Main
 {
-    // Directly reference a log4j logger.
     public static final String MODID = "enchantonce";
-    // Directly reference a slf4j logger
     private static final Logger LOGGER = LogUtils.getLogger();
-
-    //public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, MODID);
-    //public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, MODID);
-    //public static final DeferredRegister<BlockEntityType<?>> BLOCK_ENTITIES = DeferredRegister.create(ForgeRegistries.BLOCK_ENTITY_TYPES, MODID);
 
     public Main(IEventBus modEventBus, ModContainer modContainer)
     {
         initializeRegistry();
 
-        // Register the commonSetup method for modloading
         modEventBus.addListener(this::commonSetup);
 
-        // Register the Deferred Register to the mod event bus so blocks get registered
-        //BLOCKS.register(modEventBus);
-        // Register the Deferred Register to the mod event bus so items get registered
-        //ITEMS.register(modEventBus);
-        // Register the Deferred Register to the mod event bus so tabs get registered
-        //CREATIVE_MODE_TABS.register(modEventBus);
-
-        // Register ourselves for server and other game events we are interested in.
-        // Note that this is necessary if and only if we want *this* class (ExampleMod) to respond directly to events.
-        // Do not add this line if there are no @SubscribeEvent-annotated functions in this class, like onServerStarting() below.
         NeoForge.EVENT_BUS.register(this);
         EventRegistry.registerEvents();
-
-        // Register the item to a creative tab
-        //modEventBus.addListener(this::addCreative);
-
-        // Register our mod's ModConfigSpec so that FML can create and load the config file for us
-        //modContainer.registerConfig(ModConfig.Type.COMMON, Config.SPEC);
     }
 
     private void initializeRegistry()
     {
-        //ItemRegistry.initStatic();
     }
 
     private void commonSetup(final FMLCommonSetupEvent event)
