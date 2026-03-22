d24ba7a8e08fbfcc5d6b2722ad7a858129594aca
PEKAR\Sergey <sergey.pekarchik@gmail.com>
2026-03-22 21:12:03 +0100

    Replaced 'damageMainHandGear' console command with commands from The Block of Angel mod.
    (cherry picked from commit 155ebe47b571193da02cc52f438873f2637b7822)

---
+++ src/main/java/com/pekar/enchantonce/utils/Resources.java
@@ -0,0 +1,27 @@
+package com.pekar.enchantonce.utils;
+
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.equipment.EquipmentAsset;
+import net.minecraft.world.item.equipment.EquipmentAssets;
+
+public class Resources
+{
+    Resources()
+    {
+
+    }
+
+    public static ResourceLocation createResourceLocation(String namespace, String name)
+    {
+        return ResourceLocation.fromNamespaceAndPath(namespace, name); //was: new ResourceLocation(name),
+    }
+
+    public ResourceKey<EquipmentAsset> createEquipmentResourceKey(String namespace, String armorName)
+    {
+        return ResourceKey.create(
+                EquipmentAssets.ROOT_ID,
+                createResourceLocation(namespace, armorName));
+    }
+
+}
