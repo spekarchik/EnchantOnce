6cb10dae7278b7e788de30f142622e74e608e073
PEKAR\Sergey <sergey.pekarchik@gmail.com>
2026-03-26 22:27:23 +0100

    Added console commands: xp500, dayLock

---
+++ src/main/java/com/pekar/enchantonce/utils/Weather.java
@@ -0,0 +1,60 @@
+package com.pekar.enchantonce.utils;
+
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.level.Level;
+
+public class Weather
+{
+    private final Level level;
+
+    private Weather(Level level)
+    {
+        this.level = level;
+    }
+
+    public static Weather of(Level level)
+    {
+        return new Weather(level);
+    }
+
+    public void clear()
+    {
+        if (level instanceof ServerLevel serverLevel)
+        {
+            var weatherData = serverLevel.getWeatherData();
+            weatherData.setRaining(false);
+            weatherData.setThundering(false);
+        }
+    }
+
+    public void rain()
+    {
+        if (level instanceof ServerLevel serverLevel)
+        {
+            var weatherData = serverLevel.getWeatherData();
+            weatherData.setRaining(true);
+            weatherData.setThundering(false);
+            if (weatherData.getRainTime() == 0)
+            {
+                var weatherLasts = level.getRandom().nextIntBetweenInclusive(1200, 24000);
+                weatherData.setRainTime(weatherLasts);
+            }
+        }
+    }
+
+    public void thunder()
+    {
+        if (level instanceof ServerLevel serverLevel)
+        {
+            var weatherData = serverLevel.getWeatherData();
+            weatherData.setRaining(true);
+            weatherData.setThundering(true);
+            if (weatherData.getRainTime() == 0 || weatherData.getThunderTime() == 0)
+            {
+                var weatherLasts = level.getRandom().nextIntBetweenInclusive(1200, 24000);
+                weatherData.setRainTime(weatherLasts);
+                weatherData.setThunderTime(weatherLasts);
+            }
+        }
+    }
+}
