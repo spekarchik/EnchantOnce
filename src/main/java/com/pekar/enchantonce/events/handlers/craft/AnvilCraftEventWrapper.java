6784138c937904a9cfb4fab64c9e68337abe3408
PEKAR\Sergey <sergey.pekarchik@gmail.com>
2026-05-28 22:55:43 +0200

    code improvements: compatibility with Fabric [skip ci]
    (cherry picked from commit 81b2e1a717a2876a90f6348b1c1a3165dc69577d)
    (cherry picked from commit 40a84434809fc6c76940c5deb9b3cc0045a6e4d7)

--- src/main/java/com/pekar/enchantonce/events/handlers/craft/AnvilCraftEventWrapper.java
+++ src/main/java/com/pekar/enchantonce/events/handlers/craft/AnvilCraftEventWrapper.java
@@ -1,7 +1,6 @@
 package com.pekar.enchantonce.events.handlers.craft;
 
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.ItemStack;
 import net.neoforged.neoforge.event.entity.player.AnvilCraftEvent;
 
 public class AnvilCraftEventWrapper
@@ -11,11 +10,6 @@ public class AnvilCraftEventWrapper
     public AnvilCraftEventWrapper(AnvilCraftEvent event)
     {
         this.event = event;
-    }
-
-    public ItemStack getOutput()
-    {
-        return event.getOutput();
     }
 
     public Player getPlayer()
