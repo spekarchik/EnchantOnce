31e28db3cd2c7181f9e1f32bae91b6f5db20c49a
PEKAR\Sergey <sergey.pekarchik@gmail.com>
2026-06-15 23:35:18 +0200

    version specific corrections
    (cherry picked from commit 3e1a79007921fbe623e24ed9c851dc2d9e9e5a21)

--- src/main/java/com/pekar/enchantonce/mixin/AnvilMenuMixin.java
+++ src/main/java/com/pekar/enchantonce/mixin/AnvilMenuMixin.java
@@ -12,13 +12,13 @@ public class AnvilMenuMixin
     private static final int VANILLA_TOO_EXPENSIVE_LIMIT = 40;
     private static final int DISABLED_TOO_EXPENSIVE_LIMIT = Integer.MAX_VALUE;
 
-    @ModifyConstant(method = "createResult", constant = @Constant(intValue = VANILLA_TOO_EXPENSIVE_LIMIT, ordinal = 1))
+    @ModifyConstant(method = "createResultInternal", constant = @Constant(intValue = VANILLA_TOO_EXPENSIVE_LIMIT, ordinal = 1))
     private int allowRenameOnlyCostsAboveVanillaLimit(int value)
     {
         return getTooExpensiveLimit(value);
     }
 
-    @ModifyConstant(method = "createResult", constant = @Constant(intValue = VANILLA_TOO_EXPENSIVE_LIMIT, ordinal = 2))
+    @ModifyConstant(method = "createResultInternal", constant = @Constant(intValue = VANILLA_TOO_EXPENSIVE_LIMIT, ordinal = 2))
     private int allowResultCostsAboveVanillaLimit(int value)
     {
         return getTooExpensiveLimit(value);
